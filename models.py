4ea6296a7cb0866830907fa34ad46f16b1c9ff67
Ubuntu <ubuntu@ip-172-31-18-89.us-west-2.compute.internal>
2017-01-15 06:39:42 +0000

    14/01/2017

--- models.py
+++ models.py
@@ -15,8 +15,8 @@ class consultas(models.Model):
     date = fields.Date(string="Fecha de Nacimiento", required=True)
     observaciones = fields.Text()
 
-    citas_id = fields.Many2one('consultas.citas',
-        ondelete='cascade', string="Hacer cita", required=True)
+    # citas_id = fields.Many2one('consultas.citas',
+    #     ondelete='cascade', string="Hacer cita", required=True)
 
     _defaults = {
         'date': fields.Date.today(),
@@ -52,6 +52,10 @@ class citas(models.Model):
     observaciones = fields.Text();
     citas_id = fields.Many2one('consultas.consultas',
         ondelete='cascade', string="Paciente", required=True)
+    # reportes_id = fields.Many2one('consultas.reportes',
+    #     ondelete='cascade', string="Reportes")
+
+    # reportes_ids = fields.Many2many('consultas.reportes')
 
     @api.multi
     def generar_reporte(self, data=None, context=None):
@@ -68,10 +72,55 @@ class reportes(models.Model):
     _name = 'consultas.reportes'
     begin_date = fields.Date(string="Fecha inicio", required=True)
     end_date = fields.Date(string="Fecha fin", required=True)
-    paciente_id_reportes = fields.Many2one('consultas.consultas',
+    paciente = fields.Many2one('consultas.consultas',
         ondelete='cascade', string="Paciente")
     tags = fields.Text();
+    citas_ids = fields.Many2many('consultas.citas')
 
     @api.multi
     def generar_reporte_prueba(self, data=None, context=None):
-        self.env.cr.execute("some_sql", param1, param2, param3)
+        self.ensure_one()
+        queryFilter = []
+
+        #Filtros:
+        #******************************* 1)
+
+        if ((self.paciente.id == False) and (self.tags == False)):
+            print("Filer 1:")
+            # self.env.cr.execute("SELECT * FROM consultas_citas WHERE date="+str(self.begin_date))
+            # queryFilter = self.env["consultas.citas"].search_read([],['citas_id'])
+            queryFilter = self.env["consultas.citas"].search([('date', '>=', self.begin_date), ('date', '<=', self.end_date)])
+            self.env.cr.fetchall()
+
+        #******************************* 2)
+
+        elif ((self.paciente.id == False) and (self.tags != False)):
+            print("Filer 2:")
+            queryFilter = self.env["consultas.citas"].search([('date', '>=', self.begin_date), ('date', '<=', self.end_date), ('observaciones', '=ilike', self.tags)])
+            self.env.cr.fetchall()
+
+        #******************************* 3)
+
+        elif ((self.paciente.id != False) and (self.tags == False)):
+            print("Filer 3:")
+            queryFilter = self.env["consultas.citas"].search([('date', '>=', self.begin_date), ('date', '<=', self.end_date), ('citas_id', '=', self.paciente.id)])
+            self.env.cr.fetchall()
+
+        #******************************* 4)
+
+        else:
+            print("Filer 4:")
+            queryFilter = self.env["consultas.citas"].search([('date', '>=', self.begin_date), ('date', '<=', self.end_date), ('observaciones', '=ilike', self.tags), ('citas_id', '=', self.paciente.id)])
+            self.env.cr.fetchall()
+
+        idsArray = []
+        print queryFilter
+        for record in queryFilter:
+            idsArray.append(record.id)
+            print record.citas_id.name
+
+        self.citas_ids = [(6, 0, idsArray)]
+
+
+        #**** Generar reporte
+        return self.env['report'].get_action(self, "consultas.reports_template")
